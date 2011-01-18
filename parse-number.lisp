63b18a9d90c2f04c5ca8fe6d58a9afe248eb2470
Stelian Ionescu <sionescu@cddr.org>
2011-01-18 12:01:11 +0100

    Fix whitespace

--- parse-number.lisp
+++ parse-number.lisp
@@ -64,7 +64,7 @@
 			 :end end
 			 :radix radix))
     (cons integer (- end-pos start))))
-		 
+
 (defun parse-integers (string start end splitting-points &key (radix 10))
   #+optimizations
   (declare (optimize (speed 3))
@@ -309,7 +309,7 @@
 				      :start start
 				      :end end
 				      :radix radix))))))))
-		 
+
 (defparameter *test-values*
   '("1" "-1" "1034" "-364" "80/335" "3.5333" "2.4E4" "6.8d3" "#xFF" "#b-1000" "#o-101/75" "13.09s3" "35.66l5" "21.4f2" "#C(1 2)" "#c ( #xF #o-1 ) " "#c(1d1 2s1)" "#16rFF" "#9r10" "#C(#9r44/61 4f4)"))
 
