dba2205bb5c3fc4ab79cabc734bd8d8edf905cb6
Bryan Baraoidan <bryan.baraoidan@gmail.com>
2016-08-24 07:47:21 -0700

    Removed empty parentheses

--- parse-number.lisp
+++ parse-number.lisp
@@ -75,7 +75,6 @@
   (declare (optimize (speed 3) (safety 1))
            (type string string)
            (type fixnum start end radix))
-  ()
   (multiple-value-bind (integer end-pos)
       (if (= start end)
           (values 0 0)
